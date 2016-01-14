6885fd8d797bfabd6611307667b5681a234ae209
Chloe Alnaji <caedeal@gmail.com>
2016-01-14 08:27:55 -0700

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,271 @@
+var app = angular.module('sengineSite', ['ui.ace', 'ngMaterial', 'ngMessages', 'ngRoute', 'ngFileUpload', 'door3.css']);
+
+app.constant('API_URL', 'http://104.236.15.225:8080');
+
+app.factory('httpFactory', function ($http, API_URL) {
+  return {
+    hostHTML: hostHTML,
+    execute: execute
+  }
+  function hostHTML(code) {
+    return $http.post(API_URL + '/html', {"data": code});
+  }
+  function execute(code) {
+    return $http.post(API_URL + '/execute', {"data": code});
+  }
+});
+
+app.filter('reverse', function() {
+  return function(results) {
+    return results.slice().reverse();
+  };
+});
+
+//on body tag in index.html
+app.controller('GlobalController', function($scope, $location) {
+  $scope.currentTab = $location.path();
+  var start = '';
+  if($scope.currentTab == '/') {
+    start = '.root-nav';
+  }
+  if($scope.currentTab == '/editor') {
+    start = '.editor-nav';
+  }
+  if($scope.currentTab == '/hosting') {
+    start = '.hosting-nav';
+  }
+  if($scope.currentTab == '/docs') {
+    start = '.docs-nav';
+  }
+  if($scope.currentTab == '/metrics') {
+    start = '.metrics-nav';
+  }
+  // $('li').css({"border-bottom": "none"});
+  $(start).css({"border-bottom": "2px solid #dddddd"});  
+  $('li').on('click', function() {
+    $(start).css({"border-bottom": "none"});
+    $('li').css({"border-bottom": "none"});
+    $(this).css({"border-bottom": "2px solid #dddddd"});
+  })
+})
+
+app.controller('LandingController', function($scope, $css) {
+    $css.add('fonts.css');
+})
+
+app.controller('EditorController', function($scope, httpFactory, reverseFilter, $css) {
+  $css.remove('fonts.css');
+
+  setTimeout(function () {
+    $scope.$apply(function () {
+        $scope.showFlag = true;;
+    });
+  }, 0)
+
+  $scope.aceLoaded = function(_editor) {
+    _editor.getSession().setUseWorker(false);
+    _editor.renderer.session.doc.$lines[0] = "console.log('Hello world!');";
+    _editor.setOptions({
+      fontSize: 16
+    });
+    $scope.currentEditorValue = _editor.getSession().doc.$lines.join('\n');
+    httpFactory.execute($scope.currentEditorValue)
+    .then(function success(response) {
+      console.log("RESPONSE: ", response);
+      var result = {};
+      if (response.data == "language not detected") {
+        result.output = "Error: Language not detected",
+        result.language = "N/A"
+      } else {
+        if (response.data.stdout != "") {
+          result.output = response.data.stdout.trim();
+        } 
+        if (response.data.stderr != "") {
+          result.output = response.data.stderr.trim();
+        }
+        result.language = response.data.language.trim();
+      }
+      $scope.results.push(result)
+    })
+  }
+
+  $scope.aceChanged = function(e) {
+    $scope.currentEditorValue = e[1].session.doc.$lines.join('\n');
+    console.log($scope.currentEditorValue);
+  }
+
+  $scope.results = [];
+
+  $scope.clearTerminal = function() {
+    $scope.results = [];
+  }
+
+  $scope.execute = function() {
+    httpFactory.execute($scope.currentEditorValue)
+    .then(function success(response) {
+      console.log("RESPONSE: ", response);
+      var result = {};
+      if (response.data == "language not detected") {
+        result.output = "Error: Language not detected",
+        result.language = "N/A"
+      } else {
+        if (response.data.stdout != "") {
+          result.output = response.data.stdout.trim();
+        } 
+        if (response.data.stderr != "") {
+          result.output = response.data.stderr.trim();
+        }
+        result.language = response.data.language.trim();
+      }
+      $scope.results.push(result)
+    })
+  }
+
+})
+
+app.controller('HostingController', function($scope, httpFactory, Upload, $css) {
+  $css.add('fonts.css');
+
+  setTimeout(function () {
+    $scope.$apply(function () {
+        $scope.showFlag = true;
+    });
+  }, 50)
+
+  $scope.showHTMLContent = function($fileContent){
+    $scope.htmlContent = $fileContent;
+  };
+  $scope.showJSContent = function($fileContent){
+    $scope.jsContent = $fileContent;
+  };
+  $scope.showCSSContent = function($fileContent){
+    $scope.cssContent = $fileContent;
+  };
+
+  $scope.submit = function() {
+    // console.log($scope.htmlContent);
+    // console.log($scope.jsContent);
+    // console.log($scope.cssContent);
+    var head = $scope.htmlContent.split('</head>')[0] + '\n ' + '<style>\n' + String($scope.cssContent) + '\n</style></head>';
+    var body = $scope.htmlContent.split('</body>')[0] + '\n' + '<script>\n' + String($scope.jsContent) + '\n</script></body>';
+
+    $scope.compiledHtmlContent = head + '\n' + body;
+
+    // var tempElem = angular.element("<div>" + $scope.htmlContent + "</div>").css("display", "none");
+    // // tempElem.style.display = "none";
+    // // tempElem.innerHTML = $scope.htmlContent;
+    // // angular.element(tempElem);
+    // // console.log(angular.element(tempElem).html());
+    // console.log(tempElem);
+    // var head = angular.element(tempElem).find('head');
+    // console.log(head);
+
+    // if ($scope.cssContent) {
+    //   angular.element(tempElem).find("head").append("<style>" + $scope.cssContent.toString() + "</style>");
+    // }
+    // if ($scope.jsContent) {
+    //   angular.element(tempElem).find("body").append("<script>" + $scope.jsContent.toString() + "</script>");
+    // }
+    // var hostData = angular.element(tempElem).html();
+    // // tempElem.innerHTML = "";
+    // console.log(hostData)
+  }
+
+})
+
+app.controller('DocsController', function($scope, $css) {
+  $css.add('fonts.css');
+
+  setTimeout(function () {
+    $scope.$apply(function () {
+        $scope.showFlag = true;;
+    });
+  }, 50)
+})
+
+app.controller('MetricsController', function($scope, httpFactory, $route, $css) {
+  $css.add('fonts.css');
+  
+  $scope.refreshMetrics = function () {
+    $route.reload();
+  }
+  setTimeout(function () {
+    $scope.$apply(function () {
+        $scope.showBar = true;;
+    });
+  }, 0)
+  setTimeout(function () {
+    $(".progress-bar").animate({
+      width: "100%"
+    }, 1000);
+  })
+  setTimeout(function () {
+    $scope.$apply(function () {
+        $scope.showBar = false;;
+    });
+  }, 3000)
+  setTimeout(function () {
+    $scope.$apply(function () {
+        $scope.showFlag = true;
+    });
+  }, 3000)
+  // var socket = io.connect('http://localhost:3000');
+
+  // socket.emit('loadUsers');
+  // socket.emit('loadUser', 'kev');
+  // socket.emit('createUser', 'kev', 'yo');
+  // socket.emit('deleteUser', 2);
+
+  // socket.emit('loadQuestions');
+  // socket.emit('loadQuestion', 1);
+  // socket.emit('createQustion', 1, 'yo', 'ya');
+
+  // socket.emit('loadAnswers');
+  // socket.emit('loadAnswer', 1);
+  // socket.emit('createAnswer', 1, 1, 'yo', 'ya');
+
+
+  // socket.on('loadUsers', function (users) {
+  //   console.log(users); 
+  //   $scope.users = users;
+  // });
+  // socket.on('loadUser', function (user) {
+  //   console.log(user); 
+  //   $scope.user = user;
+  // });
+  // socket.on('createUser', function (user) {
+  //   console.log(user); 
+  //   $scope.user = user;
+  // });
+})
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: '/partials/landing.html',
+      controller: 'LandingController'
+    })
+    .when('/editor', {
+      templateUrl: '/partials/editor.html',
+      controller: 'EditorController'
+    })
+    .when('/hosting', {
+      templateUrl: '/partials/hosting.html',
+      controller: 'HostingController'
+    })
+    .when('/docs', {
+      templateUrl: '/partials/docs.html',
+      controller: 'DocsController'
+    })
+    .when('/metrics', {
+      templateUrl: '/partials/metrics.html',
+      controller: 'MetricsController'
+    })
+    // .when('/page-not-found', {
+    //   templateUrl: '/partials/error.html'
+    // })
+    // .otherwise({
+    //   redirectTo: '/page-not-found'
+    // });
+  $locationProvider.html5Mode(true);
+}]);
---
+++ scripts/hero.js
@@ -0,0 +1,89 @@
+var width = 550,
+    height = 50;
+
+var vertices = d3.range(100).map(function(d) {
+  return [Math.random() * width, Math.random() * height];
+});
+
+var voronoi = d3.geom.voronoi()
+    .clipExtent([[0, 0], [width, height]]);
+
+var svg = d3.select(".d3Append").append("svg")
+    // .attr("width", width)
+    // .attr("height", height)
+    .attr("viewBox", "0 0 500 500")
+    .attr("preserveAspectRatio", "xMinYMin")
+    .attr("class", "svg-content")
+    .on("mousemove", function() { vertices[0] = d3.mouse(this); redraw(); });
+
+var path = svg.append("g").selectAll("path");
+
+// var filter = svg.append("defs").append("filter").attr("id", "dropshadow").attr("height", "130%");
+// filter.append("feGaussianBlur").attr("in", "SourceAlpha").attr("stdDeviation", "3")
+// filter.append("feOffset").attr("dx", "2").attr("dy", "2").attr("result", "offsetblur")
+// var merge = filter.append("feMerge");
+// merge.append("feMergeNode");
+// merge.append("feMergeNode").attr("in", "SourceGraphic");
+
+// filters go in defs element
+var defs = svg.append("defs");
+
+// create filter with id #drop-shadow
+// height=130% so that the shadow is not clipped
+var filter = defs.append("filter")
+    .attr("id", "drop-shadow")
+    .attr("height", "130%");
+
+// SourceAlpha refers to opacity of graphic that this filter will be applied to
+// convolve that with a Gaussian with standard deviation 3 and store result
+// in blur
+filter.append("feGaussianBlur")
+    .attr("in", "SourceAlpha")
+    .attr("stdDeviation", 5)
+    .attr("result", "blur");
+
+// translate output of Gaussian blur to the right and downwards with 2px
+// store result in offsetBlur
+filter.append("feOffset")
+    .attr("in", "blur")
+    .attr("dx", 5)
+    .attr("dy", 5)
+    .attr("result", "offsetBlur");
+
+// overlay original SourceGraphic over translated blurred opacity by using
+// feMerge filter. Order of specifying inputs is important!
+var feMerge = filter.append("feMerge");
+
+feMerge.append("feMergeNode")
+    .attr("in", "offsetBlur")
+feMerge.append("feMergeNode")
+    .attr("in", "SourceGraphic");
+
+svg.append("text").attr("x", 175).attr("y", 35).attr("font-size", "30px").attr("font-weight", "bold").attr("stroke-width", 2).attr("class", "shadow").text("Sengine.io");
+svg.append("text").attr("x", 175).attr("y", 35).attr("font-size", "30px").attr("font-weight", "bold").attr("stroke-width", 2).text("Sengine.io");
+// .style("filter", "url(#drop-shadow)")
+
+svg.selectAll("circle")
+    .data(vertices.slice(1))
+  .enter().append("circle")
+    .attr("transform", function(d) { return "translate(" + d + ")"; })
+    .attr("r", 1.5);
+
+redraw();
+
+function redraw() {
+  path = path
+      .data(voronoi(vertices), polygon);
+
+  path.exit().remove();
+
+  path.enter().append("path")
+      .attr("class", function(d, i) { return "q" + (i % 9) + "-9"; })
+      .attr("d", polygon);
+
+  path.order();
+}
+
+function polygon(d) {
+  return "M" + d.join("L") + "Z";
+}
